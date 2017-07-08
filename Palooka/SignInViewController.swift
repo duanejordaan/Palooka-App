ee8f2feae9efbe107df324c44c3f4e5a1546c75e
duanejordaan <duane.jordaan@gmail.com>
2017-07-08 17:00:59 +0200

    palooka-app commit

---
+++ Palooka/SignInViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SignInViewController.swift
+//  Palooka
+//
+//  Created by Duane Jordaan on 2017/07/08.
+//  Copyright © 2017 Duane Jordaan. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        emailTextField.backgroundColor = UIColor.white
+//        emailTextField.tintColor = UIColor.white
+//        emailTextField.textColor = UIColor.gray
+//        emailTextField.attributedPlaceholder = NSAttributedString(string: emailTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+//        let bottomLayerEmail = CALayer()
+//        bottomLayerEmail.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+//        bottomLayerEmail.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+//        emailTextField.layer.addSublayer(bottomLayerEmail)
+//        
+//        passwordTextField.backgroundColor = UIColor.clear
+//        passwordTextField.tintColor = UIColor.white
+//        passwordTextField.textColor = UIColor.white
+//        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+//        let bottomLayerPassword = CALayer()
+//        bottomLayerPassword.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+//        bottomLayerPassword.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+//        passwordTextField.layer.addSublayer(bottomLayerPassword)
+        
+        
+    }
+}
---
+++ Palooka/SignUpViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SignUpViewController.swift
+//  Palooka
+//
+//  Created by Duane Jordaan on 2017/07/08.
+//  Copyright © 2017 Duane Jordaan. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
