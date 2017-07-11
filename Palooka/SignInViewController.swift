c9b72d7ab3ab93295102398a64ae28c14e3f03f8
duanejordaan <duane.jordaan@gmail.com>
2017-07-11 21:15:57 +0200

    login + signup completed with error checking and segue

--- Palooka/SignInViewController.swift
+++ Palooka/SignInViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        handleTextField()
     }
+    
+    func handleTextField() {
+        emailTextField.addTarget(self, action: #selector(SignUpViewController.textFieldDidChange), for: UIControlEvents.editingChanged)
+        passwordTextField.addTarget(self, action: #selector(SignUpViewController.textFieldDidChange), for: UIControlEvents.editingChanged)
+        
+    }
+    
+    func textFieldDidChange() {
+        guard let email = emailTextField.text, !email.isEmpty,
+            let password = passwordTextField.text, !password.isEmpty else {
+                signInButton.setTitleColor(UIColor.lightText, for: UIControlState.normal)
+                signInButton.isEnabled = false
+                return
+        }
+        
+        signInButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+        signInButton.isEnabled = true
+   }
+    
+    
+    @IBAction func signInButton_TouchUpInside(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+            if error != nil {
+                print(error!.localizedDescription)
+                return
+            }
+            self.performSegue(withIdentifier: "signInToTabbarVC", sender: nil)
+        })
+    }
+
 }
