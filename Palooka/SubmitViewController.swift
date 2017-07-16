eae5620ba9c1e0f0d0be6d5cf8357e100ff6d30d
duanejordaan <duane.jordaan@gmail.com>
2017-07-16 15:59:12 +0200

    adding submit score functionality

--- Palooka/SubmitViewController.swift
+++ Palooka/SubmitViewController.swift
@@ -7,14 +7,77 @@
 //
 
 import UIKit
+import FirebaseStorage
+import FirebaseDatabase
 
 class SubmitViewController: UIViewController {
-
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var captionTextView: UITextView!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var photoAlbum: UIImageView!
+    
+    var selectedImage: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleSelectPhoto))
+        photo.addGestureRecognizer(tapGesture)
+        photo.isUserInteractionEnabled = true
     }
+    
+    func handleSelectPhoto() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController, animated: true, completion: nil)
+        photoAlbum.isHidden = true
+    }
+        
 
+    @IBAction func submitButton_TouchUpInside(_ sender: Any) {
+        ProgressHUD.show("Waiting...", interaction: false)
+        if let profileImg = self.selectedImage, let imageData = UIImageJPEGRepresentation(profileImg, 0.1) {
+            let photoIdString = NSUUID().uuidString
+            print(photoIdString)
+            let storageRef = Storage.storage().reference(forURL: Config.STORAGE_ROOF_REF).child("posts").child(photoIdString)
+            storageRef.putData(imageData, metadata: nil, completion: { (metadata, error) in
+                if error != nil {
+                    ProgressHUD.showError(error!.localizedDescription)
+                    return
+                }
+                let photoUrl = metadata?.downloadURL()?.absoluteString
+                self.sendDataToDatabase(photoUrl: photoUrl!)
+            })
+        } else {
+            ProgressHUD.showError("Profile Image can't be empty")
+        }
+    }
    
+    func sendDataToDatabase(photoUrl: String) {
+        let ref = Database.database().reference()
+        let postsReference = ref.child("posts")
+        let newPostId = postsReference.childByAutoId().key
+        let newPostReference = postsReference.child(newPostId)
+        newPostReference.setValue(["photoUrl": photoUrl], withCompletionBlock: {
+            (error, ref) in
+            if error != nil {
+                ProgressHUD.showError(error!.localizedDescription)
+                return
+            }
+            ProgressHUD.showSuccess("Success")
+        })
+    }
+    
 }
+extension SubmitViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("did Finish Picking Media")
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+            selectedImage = image
+            photo.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
+
